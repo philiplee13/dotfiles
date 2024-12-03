85c63853bf95339fa117abd5641c6f673e9016d0
Philip Lee <philiplee@MacBookPro.lan>
2024-12-02 17:09:47 -0800

    update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-
+local action = wezterm.action
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
@@ -13,11 +13,28 @@ config.enable_tab_bar = false
 
 config.color_scheme = "Catppuccin Frappe"
 
-
 config.window_decorations = "RESIZE"
 
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
+
+-- text editing on cmd line
+config.keys = {
+	-- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = action.SendKey({
+			key = "b",
+			mods = "ALT",
+		}),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = action.SendKey({ key = "f", mods = "ALT" }),
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
